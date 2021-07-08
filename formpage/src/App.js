6f4d7fceeb4cd476688f5a62da637feb30eeb483
Arunima-Jayaraj <arunimajayaraj@gmail.com>
2021-07-08 22:47:44 +0530

    Bootstrap added,Bootstrap form added

--- formpage/src/App.js
+++ formpage/src/App.js
@@ -1,43 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
-import React,{useState} from 'react'
+import React, { useState } from "react";
 function App() {
-  let[state,setState]=useState({name:"",email:"",password:""})
-  let handlechange1=(e)=>{
-    // let a={...state}
-    // console.log(a,"event")
-    setState({...state,name:e.target.value})
-  }
-  let handlechange2=(e)=>{
-    setState({...state,email:e.target.value})
-
-  }
-  let handlechange3=(e)=>{
-    setState({...state,password:e.target.value})
-
-  }
+  let [state, setState] = useState({ name: "", email: "", password: "" });
+  let handlechange = (e) => {
+    console.log(e.target.value, "value");
+    setState({ ...state, [e.target.name]: e.target.value });
+  };
   return (
     <div className="App">
-     <div>
-        <title>Register Form</title>
-        <form onSubmit={(e)=>{e.preventDefault();console.log(state,"submit values")}}>
-          <table>
-            <tbody><tr>
-                <td>Name :</td>
-                <td><input type="text"  onChange={(e)=>handlechange1(e)}/></td>
-              </tr>
-              <tr>
-                <td>Email :</td>
-                <td><input type="email"  onChange={(e)=>handlechange2(e)}/></td>
-              </tr>
-              <tr>
-                <td>Password :</td>
-                <td><input type="password"  onChange={(e)=>handlechange3(e)}/></td>
-              </tr>
-              <tr>
-                <td><input type="submit" defaultValue="Submit" /></td>
-              </tr>
-            </tbody></table>
+      <div>
+        <h1 className="text-center">Registration</h1>
+      </div>
+      <div className="container">
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            console.log(state, "state values");
+          }}
+        >
+          <div className="form-group">
+            <label className="text-left" htmlFor="exampleInputEmail1">
+              Full Name{" "}
+            </label>
+            <input
+              name="name"
+              onClick={(e) => {
+                handlechange(e);
+              }}
+              type="text"
+              className="form-control"
+              id="exampleInputName"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="exampleInputEmail1">Email address</label>
+            <input
+              name="email"
+              onClick={(e) => {
+                handlechange(e);
+              }}
+              type="email"
+              className="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+            />
+            <small id="emailHelp" className="form-text text-muted">
+              We'll never share your email with anyone else.
+            </small>
+          </div>
+          <div className="form-group">
+            <label htmlFor="exampleInputPassword1">Password</label>
+            <input
+              onClick={(e) => {
+                handlechange(e);
+              }}
+              name="password"
+              type="password"
+              className="form-control"
+              id="exampleInputPassword1"
+            />
+          </div>
+          <button type="submit" className="btn btn-danger">
+            Submit
+          </button>
         </form>
       </div>
     </div>
